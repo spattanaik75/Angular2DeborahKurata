ad52ba00a5244fd409605182d525c5a01d909ba4
Soumya Pattanaik <SoumyaP@discovery.co.za>
2016-08-22 13:18:21 +0200

    Chapter 'Templates, Interpolation and Directives' Complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
 import {Component} from 'angular2/core'
 
+import {ProductListComponent} from 'app/products/product-list.component';
+
 @Component({
     selector : 'pm-app',
     //pm : product management
     template:`
     <div>
         <h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
+        <pm-products></pm-products>
     </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 export class AppComponent{
     pageTitle: string = 'Acme Product Management';
