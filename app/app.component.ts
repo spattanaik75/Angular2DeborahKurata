886721d437f77e2445c07d61de6a3b05944d7cfb
Soumya Ranjan Pattanaik <SoumyaP@discovery.co.za>
2016-08-22 11:11:51 +0200

    Chapter 'Introduction to Components' Complete

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector : 'pm-app',
+    //pm : product management
+    template:`
+    <div>
+        <h1>{{pageTitle}}</h1>
+        <div>My First Component</div>
+    </div>
+    `
+})
+export class AppComponent{
+    pageTitle: string = 'Acme Product Management';
+
+}
